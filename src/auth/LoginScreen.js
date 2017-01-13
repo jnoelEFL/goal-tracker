6c37e6f07fa9acbf1c4988f11ad01a6a28269c4c
Christophe Porteneuve <tdd@tddsworld.com>
2017-01-13 15:46:42 +0100

    Étape 1 : Bases HomeScreen et TrackerScreen

--- src/auth/LoginScreen.js
+++ src/auth/LoginScreen.js
@@ -9,7 +9,39 @@ import './LoginScreen.styl'
 
 export class LoginScreen extends Component {
   render() {
-    return <p>COMING SOON: LOGIN SCREEN</p>
+    return (
+      <form>
+        <Card className="loginScreen">
+          <CardTitle title="Goal Tracker" subtitle="Connexion" />
+          <CardText>
+            <TextField
+              autoFocus
+              floatingLabelText="E-mail"
+              fullWidth
+              hintText="mon@email.tld"
+              required
+              type="email"
+            />
+            <TextField
+              floatingLabelText="Mot de passe"
+              fullWidth
+              hintText="super mot de passe"
+              required
+              type="password"
+            />
+          </CardText>
+          <CardActions style={{ textAlign: 'center' }}>
+            <RaisedButton
+              icon={<ArrowForward />}
+              label="Connecte-toi"
+              labelPosition="before"
+              primary
+              type="submit"
+            />
+          </CardActions>
+        </Card>
+      </form>
+    )
   }
 }
 
