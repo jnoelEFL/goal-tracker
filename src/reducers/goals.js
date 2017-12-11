298ee600c5dabbf2c23fe9ae107e181072fbdcff
Christophe Porteneuve <tdd@tddsworld.com>
2017-12-11 18:49:15 +0100

    Début exercice étape 5 : bases pour Action Creators, Reducer et tests "Goals"

--- src/reducers/goals.js
+++ src/reducers/goals.js
@@ -11,7 +11,13 @@ const UPDATE_GOAL = 'goal-tracker/goals/GOALS_UPDATE'
 export default function reduceGoals(state = [], action) {
   switch (action.type) {
     case ADD_GOAL: {
-      // Votre code ici
+      const { name, target, units } = action.payload
+      // L’approche un poil moins « pure fonctionnelle » serait :
+      // ```js
+      // const id = Math.max(...state.map((goal) => goal.id), -1) + 1
+      // ````
+      const id = state.reduce((max, { id }) => (max > id ? max : id), -1) + 1
+      return [...state, { id, name, target, units }]
     }
 
     case REMOVE_GOAL:
